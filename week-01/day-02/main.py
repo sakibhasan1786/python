a7573fbffa4737e2755ba328bba4451e665d534b
sakibhasan1786 <sakibhasan1786@gmail.com>
2026-06-09 21:09:33 +0600

    test

---
+++ week-01/day-02/main.py
@@ -0,0 +1,22 @@
+# fname = "Sakib"
+# lname = "Hassan"
+# roll = "IT Expert"
+# cname = "Fortis Group"
+
+# print(f"I am {fname} {lname}, I am an {roll} at {cname}")
+
+
+# print("hello mr " + name)
+
+# name =input("input your name:")
+
+print("#" *30)
+print("Welcome to Band Name Generator !")
+print("#" *30)
+print("\n")
+print("\n")
+cname = input("Enter your City Name: ")
+pname = input("Enter your favourit pet name")
+print("\n")
+
+print("Congratualtions!!" + "\n" + "Your Band Name is ready..." + "\n" + "\n" +f"Your Band Nam is: {cname} {pname}" + "\n" + "Stay Tune")
